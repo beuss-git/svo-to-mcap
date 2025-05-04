8ec7c11e587e551f86e0fec33d91833b1344fad2
Benjamin Bjerken <beuss.code@gmail.com>
2025-05-04 18:08:21 +0200

    Add gsl to silence warnings and add more warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "converter.hpp"
+#include <gsl/gsl>
 #include <iostream>
 
 int main(int argc, char const* argv[])
 {
     std::string config_path = "config.yaml";
-    if (argc > 1) {
-        config_path = argv[1];
+    auto const args = gsl::span(argv, static_cast<size_t>(argc));
+    if (args.size() > 1) {
+        config_path = args[1];
     }
 
     Converter converter;
